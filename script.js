f4b77701e6fe523e3ec719f8b24c346c2ff3d99d
Jackie <jacgut19@gmail.com>
2017-12-26 17:20:46 +0000

    adding click functions

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
-/* global $ number */
+/* global $ */ /* global number */ /* global operator */ /* global newNumber */
 
 $(document).ready(function(){
 	var testNumLength = function(number) {
         if (number.length > 9) {
-            totaldiv.text(number.substr(number.length-9,9));
+            totalDiv.text(number.substr(number.length-9,9));
             if (number.length > 15) {
                 number = "";
-                totaldiv.text("Err");
+                totalDiv.text("Err");
             }
         } 
     };
-    
+   
 var Num = "number";
-var newNum = "newnumber";
+var newNum = "newNumber";
 var op = "operator";
-//var totaldiv = $("#total").text();
-var totaldiv = $("#total");
-    totaldiv.text("0");
+//var totalDiv = $("#total").text();
+var totalDiv = $("#total");
+    totalDiv.text("0");
+
+/*$("#numbers > a").not("#clear,#clearall").click(function(){
+		number += $(this).text();
+		totalDiv.text(number);
+		testNumLength(number);  */    
     
-$("#numbers a").not("#clear,#clearall").click(function(){number.append(this.html());
-            testNumLength(number);
-    });    
+$("#numbers a").not("#clear,#clearAll").click(function(){number.append(this.html());
+    testNumLength(number);
+    }); 
+    
+    $("#operators a").not("#equals").click(function(){
+       operator = $(this).text();
+        newNumber = number;
+        number = "";
+        totalDiv.text("0");
+    });
+    
+     $("#clear, #clearAll").click(function(){
+        //number="";
+        //newNumber="";
+        //totalDiv.text("0");
+        number = "";
+		totalDiv.text("0");
+		if ($(this).attr("id") === "clearall") {
+			newNumber = "";
+		}
+    });
 });
