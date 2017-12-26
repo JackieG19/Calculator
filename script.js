576852dbae2e65f21ed60afd8dec5e6bf336f89f
Jackie <jacgut19@gmail.com>
2017-12-26 14:50:08 +0000

    adding to js

---
+++ script.js
@@ -0,0 +1,18 @@
+/* global $ */
+
+$(document).ready(function(){
+	var testNumLength = function(number) {
+        if (number.length > 9) {
+            totaldiv.text(number.substr(number.length-9,9));
+            if (number.length > 15) {
+                number = "";
+                totaldiv.text("Err");
+            }
+        } 
+    };
+    
+var Num = "number";
+var newNum = "newnumber";
+var op = "operator";
+var totaldiv = $("#total").text();
+});
